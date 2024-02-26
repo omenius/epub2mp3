8ce0f324a4b662938cc741e0d2c1d5080e55cc27
omen <omen@omen>
2024-02-26 09:11:42 +0200

    upgrades, load optimization

--- epub2mp3.py
+++ epub2mp3.py
@@ -1,154 +1,161 @@
-import argparse
 import sys
-from os import path, mkdir
-from epub import extract
+if __name__ != '__main__': sys.exit()
 
-motd = "Convert epub files into audiobooks."
-arg_parser = argparse.ArgumentParser(description=motd, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-aa = arg_parser.add_argument
-aa('file', type=str, help="Epub file (input)")
-i_default = ['contents', 'copyright', 'bibliography']
-aa('-i', '--ignore', default=i_default, nargs='+', type=str.lower, help="Ignore given chapter(s)")
-aa('-f', '--filter', default='footnote', type=str.lower, help="Remove elements with matching class name(s)")
-aa('-d', '--dir', default=path.curdir, type=str, help="Output directory path")
-aa('-s', '--speed', default=1.18, type=float, help="Speaker speed: 0.0-2.0")
-aa('-n', '--name', default='Royston Min', type=str, help="Name of the speaker")
-aa('-l', '--lang', default='en', type=str.lower, help="Speaker language code")
-aa('-b', '--bitrate', default='160k', type=str.lower, help="Output bit rate: 8k-160k")
-aa('-w', '--wav', action='store_true', help="Generate a non compressed wav file")
-aa('-v', '--verbose', action='store_true', help='Increase output verbosity')
-ARGS = arg_parser.parse_args()
+def process_arguments():
+    global ARGS, AUTHOR, BOOK_NAME, YEAR, COVER, BOOK
+    import argparse as ap
+    from epub import extract
+    motd = 'Convert epub e-books into mp3 audiobooks.'
+    arg_parser = ap.ArgumentParser(description=motd, formatter_class=ap.ArgumentDefaultsHelpFormatter)
+    a = arg_parser.add_argument
+    i_default = ['contents', 'copyright', 'bibliography']
+    a('file', type=str, help='epub file (input)')
+    a('-i', '--ignore', default=i_default, nargs='+', type=str.lower, help='ignore given chapter(s)')
+    a('-f', '--filter', default='footnote', type=str.lower, help='remove elements with matching class name(s)')
+    a('-d', '--dir', default=path.curdir, type=str, help='output directory path')
+    a('-s', '--speed', default=1.18, type=float, help='speaker speed: 0.0-2.0')
+    a('-n', '--name', default='Royston Min', type=str, help='name of the speaker')
+    a('-l', '--lang', default='en', type=str.lower, help='speaker language code')
+    a('-b', '--bitrate', default='160k', type=str.lower, help='output bit rate: 8k-160k')
+    a('-w', '--wav', action='store_true', help='generate a non compressed wav file')
+    a('-v', '--verbose', action='store_true', help='increase output verbosity')
+    ARGS = arg_parser.parse_args()
 
-print(ARGS.ignore_chapter)
+    # Check args
+    rates = ['8k','16k','24k','32k','40k','48k','56k','64k','80k','96k','112k','128k','144k','160k']
+    langs = ['en','es','fr','de','it','pt','pl','tr','ru','nl','cs','ar','zh-cn','hu','ko','ja','hi']
+    if not path.exists(ARGS.file): sys.exit('Epub file not found.')
+    if not path.exists(ARGS.dir): sys.exit('Output directory not found.')
+    if ARGS.speed<0 or ARGS.speed>2: sys.exit('Speed must be between 0 and 2.')
+    if ARGS.bitrate not in rates: sys.exit('Invalid output bit rate. Valid rates are:\n'+str(rates))
+    if ARGS.lang not in langs: sys.exit('Invalid language code. Valid languages are:\n'+str(langs))
 
-# Check args
-rates = ['8k','16k','24k','32k','40k','48k','56k','64k','80k','96k','112k','128k','144k','160k']
-langs = ["en","es","fr","de","it","pt","pl","tr","ru","nl","cs","ar","zh-cn","hu","ko","ja","hi"]
-if not path.exists(ARGS.file): sys.exit('Epub file not found.')
-if not path.exists(ARGS.dir): sys.exit('Output directory not found.')
-if ARGS.speed<0 or ARGS.speed>2: sys.exit('Speed must be between 0 and 2.')
-if ARGS.bitrate not in rates: sys.exit('Invalid output bit rate. Valid rates are:\n'+str(rates))
-if ARGS.lang not in langs: sys.exit('Invalid language code. Valid languages are:\n'+str(langs))
+    try: AUTHOR, BOOK_NAME, YEAR, COVER, BOOK = extract(
+            ARGS.file, ignore_chapters=ARGS.ignore, class_filter=ARGS.filter)
+    except:
+        if ARGS.verbose: raise
+        sys.exit('Could not open the given epub file. Use -v flag to see error.')
 
-try: AUTHOR, BOOK_NAME, YEAR, COVER, BOOK = extract(
-        ARGS.file, ignore_chapters=ARGS.ignore, class_filter=ARGS.filter)
-except:
-    if ARGS.verbose: raise
-    sys.exit('Could not open the given epub file. Use -v flag to see error.')
+    if path.exists(path.join(ARGS.dir, BOOK_NAME)):
+        sys.exit(f'Name collision. Directory already exists: {BOOK_NAME}')
 
-if path.exists(path.join(ARGS.dir, BOOK_NAME)):
-    sys.exit(f'Name collision. Directory already exists: {BOOK_NAME}')
+def load_model():
+    global MODEL, GPT_COND_LATENT, SPEAKER_EMBEDDING
+    from TTS.tts.configs.xtts_config import XttsConfig
+    from TTS.tts.models.xtts import Xtts
+    from TTS.utils.manage import ModelManager
+    from TTS.utils.generic_utils import get_user_data_dir
+    from torch import cuda
+    model_name = 'tts_models/multilingual/multi-dataset/xtts_v2'
+    ModelManager().download_model(model_name)
+    model_path = path.join(get_user_data_dir('tts'), model_name.replace('/', '--'))
+    config = XttsConfig()
+    config.load_json(path.join(model_path, 'config.json'))
+    MODEL = Xtts.init_from_config(config)
+    MODEL.load_checkpoint(config, checkpoint_dir=model_path)
+    if cuda.is_available(): MODEL.cuda()
+    else: print('Cuda GPU not available. Using CPU.')
 
-# Load libs
-print('Loading...')
-from split_sentence import split_sentence
-from TTS.tts.configs.xtts_config import XttsConfig
-from TTS.tts.models.xtts import Xtts
-from TTS.utils.manage import ModelManager
-from TTS.utils.generic_utils import get_user_data_dir
-from scipy.io.wavfile import write
-from torch import cuda
-import numpy as np
-if not ARGS.wav:
-    from io import BytesIO
-    from pydub import AudioSegment
-    import eyed3; eyed3.log.setLevel("ERROR")
+    # Select a valid speaker
+    while ARGS.name not in MODEL.speaker_manager.speakers.keys():
+        print(f'Invalid speaker name: {ARGS.name}\n')
+        print('Valid names are: ')
+        for key in MODEL.speaker_manager.speakers: print(f'{key}, ', end='')
+        ARGS.name = input('\n\nEnter a valid name: ')
 
-# Load models
-model_name = 'tts_models/multilingual/multi-dataset/xtts_v2'
-ModelManager().download_model(model_name)
-model_path = path.join(get_user_data_dir('tts'), model_name.replace('/', '--'))
-config = XttsConfig()
-config.load_json(path.join(model_path, 'config.json'))
-model = Xtts.init_from_config(config)
-model.load_checkpoint(config, checkpoint_dir=model_path)
-if cuda.is_available(): model.cuda()
-else: print('Cuda GPU not available. Using CPU.')
+    GPT_COND_LATENT, SPEAKER_EMBEDDING = MODEL.speaker_manager.speakers[ARGS.name].values()
 
-# Select a valid speaker
-while ARGS.name not in model.speaker_manager.speakers.keys():
-    print(f'Invalid speaker name: {ARGS.name}\n')
-    print('Valid names are: ')
-    for key in model.speaker_manager.speakers: print(f'{key}, ', end='')
-    ARGS.name = input('\n\nEnter a valid name: ')
-
-# Returns a numpy array of silence for given time
+# Returns a numpy array of silence for a given time
 def get_pause(sec):
-    pause = int(sec*24000)
-    return np.zeros(pause, dtype=np.float32)
+    return np.zeros(int(sec*24000), dtype=np.float32)
 
-gpt_cond_latent, speaker_embedding = model.speaker_manager.speakers[ARGS.name].values()
-# Returns a numpy array of speech from given text
-def get_wav(text):
+# Returns a numpy array of speech from a given text
+def get_speech(text):
     sentences = split_sentence(text, ARGS.lang)
     wavs = []
     for sentence in sentences:
-        # import re; sentence = re.sub("([^\x00-\x7F]|\w)(\.|\。|\?)",r"\1 \2\2", sentence)
+        # import re; sentence = re.sub('([^\x00-\x7F]|\w)(\.|\。|\?)',r'\1 \2\2', sentence)
         try:
-            wavs.append(model.inference(
+            wavs.append(MODEL.inference(
                 sentence,
                 ARGS.lang,
-                gpt_cond_latent,
-                speaker_embedding,
+                GPT_COND_LATENT,
+                SPEAKER_EMBEDDING,
                 speed=ARGS.speed,
                 repetition_penalty=5.0,
                 enable_text_splitting = False,
             )['wav'])
         except:
-            print("error with sentence: "+sentence)
+            print('error with sentence: ' + sentence)
             raise
 
         wavs.append(get_pause(0.6))
     return np.concatenate(wavs)
 
-# == MAIN ==================================================================
-print(" ┌┐┌┐╷╷├┐─┐──────┐\n ├┘├┘└┘└┘┌┘┌┬┐┌┐─┤\n └─╵─────└─╵╵╵├┘─┘")
+# === Main function ==========================================================
+def main():
+    try: mkdir(path.join(ARGS.dir, BOOK_NAME))
+    except:
+        if ARGS.verbose: raise
+        sys.exit(f'Cant create a direcotry in "{ARGS.dir}". Use -v flag to see error.')
 
-try: mkdir(path.join(ARGS.dir, BOOK_NAME))
-except:
-    if ARGS.verbose: raise
-    sys.exit(f'Cant create a direcotry in "{ARGS.dir}". Use -v flag to see error.')
+    for index, (title, texts) in enumerate(BOOK):
+        print(f'Converting chapter {index+1} of {len(BOOK)}: {title}')
+        file_path = path.join(ARGS.dir, BOOK_NAME, title)
 
-for index, (title, texts) in enumerate(BOOK):
-    print('Converting chapter: '+title)
-    file_path = path.join(ARGS.dir, BOOK_NAME, title)
-
-    # Generate audio
-    wavs = []
-    wavs.append(get_pause(1))
-    for text in texts:
-        if text.isspace(): continue
-        wavs.append(get_wav(text))
+        # Generate audio
+        wavs = []
         wavs.append(get_pause(1))
-    wavs.append(get_pause(2))
-    wav = np.concatenate(wavs)
+        for text in texts:
+            if text.isspace(): continue
+            wavs.append(get_speech(text))
+            wavs.append(get_pause(1))
+        wavs.append(get_pause(2))
+        wav = np.concatenate(wavs)
 
-    # Save to wav
-    if ARGS.wav:
-        write(file_path+'.wav', 24000, wav)
-        continue
+        # Save to wav
+        if ARGS.wav:
+            write(file_path+'.wav', 24000, wav)
+            continue
 
-    # Convert to mp3 and save
-    memoryBuff = BytesIO()
-    write(memoryBuff, 24000, wav)
-    AudioSegment.from_wav(memoryBuff).export(file_path+'.mp3', format='mp3', bitrate=ARGS.bitrate)
-    memoryBuff.close()
+        # Convert to mp3 and save
+        memoryBuff = BytesIO()
+        write(memoryBuff, 24000, wav)
+        AudioSegment.from_wav(memoryBuff).export(file_path+'.mp3', format='mp3', bitrate=ARGS.bitrate)
+        memoryBuff.close()
 
-    # Write mp3 metadata
-    mp3 = eyed3.load(file_path+'.mp3')
-    mp3.initTag()
-    mp3.tag.track_num = index+1
-    mp3.tag.title = title
-    mp3.tag.album = BOOK_NAME
-    mp3.tag.artist = AUTHOR or 'Unknown'
-    mp3.tag.release_date = eyed3.core.Date(YEAR or 1970)
-    if COVER:
-        mp3.tag.images.set(
-            type_=3,
-            img_data=COVER.content,
-            mime_type=COVER.media_type,
-            description='Cover image'
-        )
-    mp3.tag.save()
+        # Write mp3 metadata
+        mp3 = eyed3.load(file_path+'.mp3')
+        mp3.initTag()
+        mp3.tag.track_num = index+1
+        mp3.tag.title = title
+        mp3.tag.album = BOOK_NAME
+        mp3.tag.artist = AUTHOR or 'Unknown'
+        mp3.tag.release_date = eyed3.core.Date(YEAR or 1970)
+        if COVER:
+            mp3.tag.images.set(
+                type_=3,
+                img_data=COVER.content,
+                mime_type=COVER.media_type,
+                description='Cover image'
+            )
+        mp3.tag.save()
 
-print('Job finished.')
+    print('Job finished.')
+
+try:
+    from os import path, mkdir
+    process_arguments()
+    print(' ┌┐┌┐╷╷├┐┌┐──────┐\n ├┘├┘└┘└┘┌┘┌┬┐┌┐─┤\n └─╵─────└─╵╵╵├┘─┘\nLoading...')
+    import numpy as np
+    from split_sentence import split_sentence
+    from scipy.io.wavfile import write
+    if not ARGS.wav:
+        from io import BytesIO
+        from pydub import AudioSegment
+        import eyed3; eyed3.log.setLevel('ERROR')
+    
+    load_model()
+    main()
+
+except KeyboardInterrupt: sys.exit('Sopped by user.')
